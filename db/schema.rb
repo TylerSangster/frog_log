fb8ecdf8ca120be1e6ed034053958a68df29992a
NajwaAzer <najwa.azer@gmail.com>
2013-08-22 05:02:40 +0000

    Delete unused auto-generated RSpec files

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20130821203801) do
-=======
 ActiveRecord::Schema.define(version: 20130821211220) do
->>>>>>> voting-on-reviews
 
   create_table "resources", force: true do |t|
     t.string   "name"
