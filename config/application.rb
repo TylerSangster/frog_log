d60c02a67cdf6f7cd2fb8909ca94664499a1fa40
Tyler Sangster <tylersangster@gmail.com>
2013-09-07 15:52:00 -0400

    fixing precompiling images

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,6 @@ module Frog
 
     config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-    #config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
   end
 end
