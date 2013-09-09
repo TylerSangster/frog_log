20dddb831c78c5ecaccd3a79bab2da71bec03cb7
Sam <samanthakpower@gmail.com>
2013-09-09 02:08:27 -0400

    Reactivated rmagick

--- app/uploaders/resource_photo_uploader.rb
+++ app/uploaders/resource_photo_uploader.rb
@@ -3,10 +3,10 @@
 class ResourcePhotoUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  #include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
   
-  #process :resize_to_fill => [200,200]
+  process :resize_to_fill => [200,200]
 
   # Choose what kind of storage to use for this uploader:
   storage :file
